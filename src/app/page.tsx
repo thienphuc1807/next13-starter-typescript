1694b738b767b5140f24740cf243c76a265b3544
thienphuc1807 <90585795+thienphuc1807@users.noreply.github.com>
2023-10-05 08:07:29 +0700

    useCSSmodule

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import style1 from "@/styles/app.module.css";
+import style2 from "@/styles/cikey.module.css";
 
 export default function Home() {
     return (
         <main className={styles.main}>
             <div className={styles.description}>
                 <p>Hello CiKey welcome to NextJS 13</p>
-                <Link href="/facebook">Facebook</Link>
+                <Link href="/facebook" className={style1["red"]}>
+                    <span className={style2["red"]}>Facebook</span>
+                </Link>
                 <Link href="/tiktok">Tiktok</Link>
                 <Link href="/youtube">Youtube</Link>
                 <div>
