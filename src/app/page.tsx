a2462518cc5194d20c4c23d5aeb1c01b30407702
thienphuc1807 <90585795+thienphuc1807@users.noreply.github.com>
2023-10-06 15:06:41 +0700

    useSWRandTypeScript

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,26 +5,45 @@
 // import style2 from "@/styles/cikey.module.css";
 import Table from "@/components/Table";
 import { useEffect } from "react";
+import useSWR from "swr";
 
 export default function Home() {
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch("http://localhost:8000/blogs");
-            const blogs = await response.json();
-            console.log(blogs);
-        };
-        fetchData();
-    }, []);
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const response = await fetch("http://localhost:8000/blogs");
+    //         const blogs = await response.json();
+    //         console.log(blogs);
+    //     };
+    //     fetchData();
+    // }, []);
+
+    const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+    const { data, error, isLoading } = useSWR(
+        " http://localhost:8000/blogs",
+        fetcher,
+        {
+            revalidateIfStale: false,
+            revalidateOnFocus: false,
+            revalidateOnReconnect: false,
+        }
+    );
+
+    if (!data) {
+        return <div>...loading</div>;
+    }
+
     return (
         <main>
             <div>
+                {data?.length}
                 <p>Hello CiKey welcome to NextJS 13</p>
                 {/* <Link href="/facebook" className={style1["red"]}>
                     <span className={style2["red"]}>Facebook</span>
                 </Link>
                 <Link href="/tiktok">Tiktok</Link>
                 <Link href="/youtube">Youtube</Link> */}
-                <Table />
+                <Table blogs={data} />
                 {/* <div>
                     <a
                         href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,34 +1,37 @@
-'use client'
-import { Table } from "react-bootstrap";
-function AppTable() {
+"use client";
+import { Table, Button } from "react-bootstrap";
+interface IProps {
+    blogs: IBlog[];
+}
+function AppTable(props: IProps) {
+    const { blogs } = props;
     return (
         <Table striped bordered hover>
             <thead>
                 <tr>
-                    <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Username</th>
+                    <th>No</th>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Action</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td colSpan={2}>Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr>
+                {blogs?.map((blog) => {
+                    return (
+                        <tr key={blog.id}>
+                            <td>{blog.id}</td>
+                            <td>{blog.title}</td>
+                            <td>{blog.author}</td>
+                            <td>
+                                <Button>View</Button>
+                                <Button variant="warning" className="mx-3">
+                                    Edit
+                                </Button>
+                                <Button variant="danger">View</Button>
+                            </td>
+                        </tr>
+                    );
+                })}
             </tbody>
         </Table>
     );
